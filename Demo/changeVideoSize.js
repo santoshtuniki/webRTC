79135cdeef20fc4050f5b690ee33ae718a2f7619
Saisantoshkumar Tuniki <saisantosh.tuniki@onpassive.com>
2024-02-20 12:22:45 +0530

    Change Screen Size added

--- Demo/changeVideoSize.js
+++ Demo/changeVideoSize.js
@@ -1,9 +1,23 @@
 const supportedConstraints = navigator.mediaDevices.getSupportedConstraints();
 
 const changeVideoSize = () => {
-    stream.getTracks().forEach((track) => {
+    stream.getVideoTracks().forEach(track => {
+        const height = document.getElementById("vid-height").value;
+        const width = document.getElementById("vid-width").value;
         const capabilities = track.getCapabilities();
         console.log(capabilities);
+        
+        const vConstraints = {
+            height: {
+                exact: height < capabilities.height.max ? height : capabilities.height.max
+            },
+            width: {
+                exact: width < capabilities.width.max ? width : capabilities.width.max
+            },
+            // frameRate: 5,
+            // aspectRatio: 10  // Ratio of width to height
+        };
+        track.applyConstraints(vConstraints)
     })
 }
 
