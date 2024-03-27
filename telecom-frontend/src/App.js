2d0dde1afa32e1c8c1930a78eaafe17cc00e9870
Saisantoshkumar Tuniki <saisantosh.tuniki@onpassive.com>
2024-03-27 11:19:00 +0530

    Routes added

--- telecom-frontend/src/App.js
+++ telecom-frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+// module imports
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+// css imports
 import './App.css';
+
+// util imports
 import socketConnection from './utils/socketConnection';
 
-function App() {
+// component imports
+import MainVideoPage from './videoComponents/MainVideoPage';
+
+const Home = () => <h1>Hello, Home page</h1>
+
+const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route exact path='/' Component={Home} />
+                <Route path='/join-video' Component={MainVideoPage} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
