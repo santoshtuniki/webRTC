4b7e76d7c9b199888a810b96c1f41eca21a8fc0b
Saisantoshkumar Tuniki <saisantosh.tuniki@onpassive.com>
2024-03-27 10:40:24 +0530

    JWT token authentication added

--- telecom-backend/expressRoutes.js
+++ telecom-backend/expressRoutes.js
@@ -1,7 +1,28 @@
 // All our express stuff happens here (Routes)
 
 const app = require("./server").app;
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
 
-app.get("/test", (req, res) => {
-    res.send("This is a test router");
+app.get("/user-link", (req, res) => {
+    // data for end-user's appointment
+    const apptData = {
+        professionalFullName: "Santosh T",
+        apptDate: Date.now()
+    };
+
+    // encode this data into a token
+    const token = jwt.sign(apptData, process.env.SECRET_KEY);
+
+    res.send(`https://localhost:3000/join-video?token=${token}`);
+})
+
+app.get("/validate-link", (req, res) => {
+    // get token from url
+    const token = req.query.token;
+
+    // decode the token
+    const decodedData = jwt.verify(token, process.env.SECRET_KEY);
+
+    res.send(decodedData);
 })
